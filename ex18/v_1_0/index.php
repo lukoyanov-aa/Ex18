a9bdb79eb47eacdb0053e4ee55dc6ddb5dfb9159
Lukoyanov84 <Lukoyanov_aa@mail.ru>
2018-08-14 14:23:22 +0300

    Структурирование JS и CSS

--- ex18/v_1_0/index.php
+++ ex18/v_1_0/index.php
@@ -10,7 +10,7 @@
     <title>Рейтинг (пример 18)</title>
 
     <!-- Bootstrap -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="../../vendor/css/bootstrap/v3.1.1/bootstrap.min.css" rel="stylesheet">
     <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css" rel="stylesheet">
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
@@ -21,9 +21,9 @@
     <![endif]-->
 	
 	<!-- Include roboto.css to use the Roboto web font, material.css to include the theme and ripples.css to style the ripple effect -->
-	<link href="css/roboto.min.css" rel="stylesheet">
-	<link href="css/material.min.css" rel="stylesheet">
-	<link href="css/ripples.min.css" rel="stylesheet">
+        <link href="../../vendor/css/roboto/roboto.min.css" rel="stylesheet">
+        <link href="../../vendor/css/material/material.min.css" rel="stylesheet">
+        <link href="../../vendor/css/ripples/ripples.min.css" rel="stylesheet">
 	
     <link href="css/application.css" rel="stylesheet">
 
@@ -109,11 +109,11 @@
 </div>
 
 <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-<script type="text/javascript" src="js/bootstrap.min.js"></script>
-<script type="text/javascript" src="js/tempusjs.min.js"></script>
-<script type="text/javascript" src="js/accounting.min.js"></script>
-<script src="js/ripples.min.js"></script>
-<script src="js/material.min.js"></script>
+<script type="text/javascript" src="../../vendor/js/bootstrap/v3.1.1/bootstrap.min.js"></script>
+<script type="text/javascript" src="../../vendor/js/tempusjs/v0.2.18/tempusjs.min.js"></script>
+<script type="text/javascript" src="../../vendor/js/accounting/v0.4.2/accounting.min.js"></script>
+<script src="../../vendor/js/ripples/ripples.min.js"></script>
+<script src="../../vendor/js/material/material.min.js"></script>
 <script type="text/javascript" src="js/application.js"></script>
 <script src="//api.bitrix24.com/api/v1/"></script>
 
--- ex18/v_1_0/install.php
+++ ex18/v_1_0/install.php
@@ -7,7 +7,7 @@
     <title>Установка "Рейтинг" (пример 18)</title>
 
     <!-- Bootstrap -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="../../vendor/css/bootstrap/v3.1.1/bootstrap.min.css" rel="stylesheet">
     <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css" rel="stylesheet">
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
@@ -18,9 +18,9 @@
     <![endif]-->
 	
 	<!-- Include roboto.css to use the Roboto web font, material.css to include the theme and ripples.css to style the ripple effect -->
-	<link href="css/roboto.min.css" rel="stylesheet">
-	<link href="css/material.min.css" rel="stylesheet">
-	<link href="css/ripples.min.css" rel="stylesheet">
+        <link href="../../vendor/css/roboto/roboto.min.css" rel="stylesheet">
+        <link href="../../vendor/css/material/material.min.css" rel="stylesheet">
+        <link href="../../vendor/css/ripples/ripples.min.css" rel="stylesheet">
 	
     <link href="css/application.css" rel="stylesheet">
 
@@ -50,10 +50,10 @@
 </div>
 
 <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-<script type="text/javascript" src="js/bootstrap.min.js"></script>
-<script type="text/javascript" src="js/tempusjs.min.js"></script>
-<script src="js/ripples.min.js"></script>
-<script src="js/material.min.js"></script>
+<script type="text/javascript" src="../../vendor/js/bootstrap/v3.1.1/bootstrap.min.js"></script>
+<script type="text/javascript" src="../../vendor/js/tempusjs/v0.2.18/tempusjs.min.js"></script>
+<script src="../../vendor/js/ripples.min.js"></script>
+<script src="../../vendor/js/material.min.js"></script>
 <script type="text/javascript" src="js/application.js"></script>
 <script src="//api.bitrix24.com/api/v1/"></script>
 
